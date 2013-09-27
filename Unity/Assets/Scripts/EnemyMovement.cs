d6f311eb1696e44319faa023583d5f908467c5e9
Alexander Solan <alexander.solan@gmail.com>
2013-09-27 23:24:01 +0300

    Enemy targetting and spawning.

---
+++ Unity/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+
+	public float speed = 1f;
+	// Use this for initialization
+	void Start () {
+		
+		
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		transform.position = Vector3.MoveTowards(transform.position, new Vector3 (0,0,0), Time.deltaTime * speed);
+		
+	}
+}
---
+++ Unity/Assets/Scripts/SpawnScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnScript : MonoBehaviour {
+	
+	Random rand;
+	public GameObject[] enemyPreFab;
+	public GameObject LeftEdge, RightEdge;
+	
+	private float spawnCounter;
+	
+	public float enemySpawnMax = 5f;
+	public float enemySpawnMin = 0.5f;
+	public bool canSpawn = true;
+	
+	// Use this for initialization
+	void Start () {
+		
+		spawnCounter = Random.Range(enemySpawnMin, enemySpawnMax);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (canSpawn) 
+		{
+			if (spawnCounter > 0)
+			{
+				spawnCounter -= Time.deltaTime;
+			}
+			else
+			{
+				Instantiate (enemyPreFab[Random.Range(0,enemyPreFab.Length)], 
+					new Vector3(Random.Range(LeftEdge.transform.position.x, RightEdge.transform.position.x), 
+					Random.Range(LeftEdge.transform.position.y, RightEdge.transform.position.y), 0), Quaternion.identity);
+				spawnCounter = Random.Range (enemySpawnMin, enemySpawnMax);
+			}
+		}
+		
+	}
+}
